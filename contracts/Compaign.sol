bcff8458543174e212a49ed665a0d81e0798b618
root <root@workspacem43dmqs0pcpo41z1.sbweb3j-68c96bb5ff-pblz8>
2019-01-30 02:33:28 +0000

    First commit

---
+++ contracts/Compaign.sol
@@ -0,0 +1,87 @@
+pragma solidity 0.4.25;
+
+
+contract CampaignCreator {
+    address[] public campaigns;
+    
+    function createCampaign(uint minContribution, string description) 
+        public {
+            address newCampaign = new CrowdCollab(
+                msg.sender,
+                minContribution,
+                description);
+
+            campaigns.push(newCampaign);
+    }
+    
+    function getCampaigns() public view returns( address[] ) {
+        return campaigns;
+    }
+}
+
+contract CrowdCollab { 
+    
+    struct Request {
+        string description;
+        uint amount;
+        address recipient;
+        bool complete;
+        mapping(address=>bool) approvals;
+        uint approvalCount; 
+    }
+address public manager;
+    uint public minimumContribution;
+    string public campaignDescription;
+    mapping(address=>bool) public supporters;
+    uint public numberSupporters;
+    Request[] public requests;
+
+    modifier managerOnly() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    modifier supporterOnly() {
+        require(supporters[msg.sender]);
+        _;
+    }
+
+    constructor(address creator, uint minContribution, string description) public {  
+        manager = creator;
+        minimumContribution = minContribution;
+        campaignDescription = description;
+    }
+
+    function support() public payable {
+        require(msg.value > minimumContribution);
+        supporters[msg.sender] = true;
+        numberSupporters ++;
+    }      
+
+    function createRequest(string description, uint amount, address recipient) 
+        public managerOnly {
+        Request memory newRequest = Request({
+            description: description,              
+            amount: amount,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+        requests.push(newRequest);
+    }
+    
+    function approveRequest(uint requestId) public supporterOnly {
+        Request storage request = requests[requestId];   
+        require(!request.approvals[msg.sender]);
+        request.approvals[msg.sender] = true;
+        request.approvalCount ++;
+    }  
+
+    function finalizeRequest(uint requestId) public managerOnly {
+        Request storage request = requests[requestId];
+        require(!request.complete);    
+        require(request.approvalCount > (numberSupporters/2));
+        request.recipient.transfer(request.amount);
+        request.complete = true;
+    } 
+}
