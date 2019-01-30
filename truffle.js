31caa4367e3b7276bf4163e7783dfdf57509068f
Duong Van Sang <sangdv2703@gmail.com>
2019-01-30 22:12:48 +0700

    fix truffle migrate issue

--- truffle.js
+++ truffle.js
@@ -1,48 +1,58 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura API
- * keys are available for free at: infura.io/register
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
+/*
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * function when declaring them. Failure to do so will cause commands to hang. ex:
+ * ```
+ * mainnet: {
+ *     provider: function() { 
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     },
+ *     network_id: '1',
+ *     gas: 4500000,
+ *     gasPrice: 10000000000,
+ *   },
  */
 
-const PrivateKeyProvider = require("truffle-privatekey-provider");
-const networkRPCURL = "http://bops-t.morpheuslabs.io:26918";
-const privateKey = "0xc115f2ca701618bfc73126f1cac91beba7a59511801df72ee41465ddef9931a5";
 
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const PrivateKeyProvider = require("truffle-privatekey-provider");
+
+const gasPrice = 500000000; //process.env.GASPRICE;
+const key = "c115f2ca701618bfc73126f1cac91beba7a59511801df72ee41465ddef9931a5";
+const url = "https://demo-26918-test.morpheuslabs.io"
+
+// const getGasPrice = async function() {
+//   let err, res;
+//   [err, res] = await to (axios.get('https://www.etherchain.org/api/gasPriceOracle')).then(response => response.data.standard);
+//
+//   if (err) {
+//     gasPrice = 10000000000;
+//   } else {
+//     gasPrice = res;
+//   }
+// };
+//
+// getGasPrice();
+
+console.log('Gas Price: ' + gasPrice + ' Wei');
 
 module.exports = {
-  networks: {
-    development: {
-        provider: function() {
-            return new PrivateKeyProvider(privateKey, networkRPCURL)
-        },
-        network_id: 1, 
-        gas: 6000000,
-        gasPrice: 500000000000
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   },
-
-  compilers: {
-    solc: {
-       settings: {          // See the solidity docs for advice about optimization and evmVersion
-        optimizer: {
-          enabled: true,
-          runs: 200
-        }
-       }
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {              // ganache development
+      provider: function() {
+        // return new HDWalletProvider(mnemonic, infuraMain)
+        return new PrivateKeyProvider(key, url)
+      },
+      network_id: "*",
+      gas: 6000000,
+      gasPrice: gasPrice
     }
   }
-}
+};
