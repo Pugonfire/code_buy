6563d00498c2b650b23821883ab5339b0e1d5e75
Duong Van Sang <sangdv2703@gmail.com>
2019-01-31 14:51:06 +0700

    Add comments for smart contract

--- contracts/CampaignCreator.sol
+++ contracts/CampaignCreator.sol
@@ -1,22 +1,34 @@
-pragma solidity ^0.4.25;
+pragma solidity 0.4.25;
 
 import "./CrowdCollab.sol";
 
+/**
+ * @dev: Campaign Factory contract to generate Crowdfund smart contract to run Crowd fund
+ */
 contract CampaignCreator {
+
+    // list of campaigns
     address[] public campaigns;
     
-    function createCampaign(uint minContribution, string description) 
-        public {
-            address newCampaign = new CrowdCollab(
-                msg.sender,
-                minContribution,
-                description
-            );
+    /**
+     * @dev: Create new campaign and send default manager is caller
+     * @param minContribution minimum money can contribute for project (in ETH)
+     * @param description description of campaign, purpose of campaign
+     */
+    function createCampaign(uint minContribution, string description) public {
+        address newCampaign = new CrowdCollab(
+            msg.sender,
+            minContribution,
+            description
+        );
 
-            campaigns.push(newCampaign);
+        campaigns.push(newCampaign);
     }
-    
-    function getCampaigns() public view returns( address[] ) {
+
+    /**
+     * @dev: get all campaigns
+     */
+    function getCampaigns() public view returns (address[]) {
         return campaigns;
     }
 }
--- contracts/CrowdCollab.sol
+++ contracts/CrowdCollab.sol
@@ -1,6 +1,15 @@
-pragma solidity ^0.4.25;
+pragma solidity 0.4.25;
 
+/**
+ * @title Crowd fund smart contract, demonstration to solve problem of center crowd fund model
+ * @author Duong Van Sang
+ * @dev Crowd Colla is smart contract is contract that allow creators and entrepreneurs to get fund
+ * from supporters for their project, and also let supporter join in approving their expense items
+ */
 contract CrowdCollab { 
+    /**
+     * Request expense detail information
+     */
     struct Request {
         string description;
         uint amount;
@@ -10,11 +19,23 @@ contract CrowdCollab {
         uint approvalCount; 
     }
 
+    // address of manager, who manages campaign
     address public manager;
+
+    // min contribution value in ETH
     uint public minimumContribution;
+
+    // description about your campaign
     string public campaignDescription;
+
+    // a mapping list of supporter address, to check if address is supporter or not
     mapping(address=>bool) public supporters;
+
+    // current number of supporter of campaign, 
+    // when any one contribute money for project, he/she will be come supporter
     uint public numberSupporters;
+
+    // list of expense request for project
     Request[] public requests;
 
     modifier managerOnly() {
@@ -27,22 +48,37 @@ contract CrowdCollab {
         _;
     }
 
+    /**
+     * @dev: init campaign 
+     * @param creator who is author and manage project
+     * @param minContribution minimum money can contribute for project (in ETH)
+     * @param description description of campaign, purpose of campaign
+     */
     constructor(address creator, uint minContribution, string description) public {  
         manager = creator;
         minimumContribution = minContribution;
         campaignDescription = description;
     }
 
+    /**
+     * @dev: payable of campaign, where supporter send Ethereum to campaign
+     */
     function support() public payable {
         require(msg.value > minimumContribution);
         supporters[msg.sender] = true;
         numberSupporters ++;
     }      
 
+    /**
+     * @dev: Create a expense request, only manager of campaign are able to do it
+     * @param description expense purpose
+     * @param amount total money need for expense items
+     * @param recipient who, which address will reiceive money when request is approved
+     */
     function createRequest(string description, uint amount, address recipient) 
         public managerOnly {
         Request memory newRequest = Request({
-            description: description,              
+            description: description,
             amount: amount,
             recipient: recipient,
             complete: false,
@@ -51,6 +87,10 @@ contract CrowdCollab {
         requests.push(newRequest);
     }
     
+    /**
+     * @dev: Approve expense request from manager, apply for supporter only
+     * @param requestId expense request id
+     */
     function approveRequest(uint requestId) public supporterOnly {
         Request storage request = requests[requestId];   
         require(!request.approvals[msg.sender]);
@@ -58,6 +98,10 @@ contract CrowdCollab {
         request.approvalCount ++;
     }  
 
+    /**
+     * @dev: finalize expense request and transfer money to receiver
+     * @param requestId expense request id
+     */
     function finalizeRequest(uint requestId) public managerOnly {
         Request storage request = requests[requestId];
         require(!request.complete);    
